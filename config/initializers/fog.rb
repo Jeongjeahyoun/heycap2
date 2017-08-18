2dc7eaf25d37c777bfc7700ad3de0704edafea9f
jeongjeahyoun <zeni9@naver.com>
2017-08-18 18:35:09 +0000

    44

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              '',                        # required
-    aws_access_key_id:     '',                        # required
+    aws_access_key_id:     'AKIAJ2D4OT3YSQFRGYLA',                        # required
     aws_secret_access_key: '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',                        # required
     region:                'ap-northeast-2',             # 아시아-한국서버 명칭
     endpoint:              'https://s3-ap-northeast-2.amazonaws.com' # 아시아-한국서버
